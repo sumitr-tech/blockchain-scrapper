6cba81acec884f3d27fdac348cce799249184c52
Sumit Rajput <sumitr@techracers.com>
2017-10-11 20:40:57 +0530

    Added parsing to get respective blockchain adapter

--- src/parser/index.js
+++ src/parser/index.js
@@ -1,3 +1,32 @@
-export const parseConfig = (config) => {
+import {
+  EthereumBlockchain,
+  BitcoinBlockchain,
+  TokenEthereumBlockchain
+} from '../constants'
 
+import Adapter from '../blockchain-adapters'
+
+export const parseConfig = (config) => {
+  return config.map(config => {
+    const adapter = getBlockchainAdopter(config)
+    return {
+      blockchain: config.blockchain,
+      adapter
+    }
+  })
+}
+
+const getBlockchainAdopter = (config) => {
+  const { blockchain, zValue, toAddress, clientNode, contractAddress } = config
+
+  switch (blockchain) {
+    case EthereumBlockchain:
+      return new Adapter.Bitcoin(toAddress, clientNode, zValue)
+    case BitcoinBlockchain:
+      return new Adapter.Ethereum(toAddress, clientNode, zValue)
+    case TokenEthereumBlockchain:
+      return new Adapter.TokenETH(toAddress, clientNode, zValue, contractAddress)
+    default:
+      return undefined
+  }
 }
