c531d15b72487047a04914850fe55814006e32b5
Sumit Rajput <sumitr@techracers.com>
2017-10-13 10:43:49 +0530

    Fixed lint issues

--- src/blockchain-adapters/ethereum.js
+++ src/blockchain-adapters/ethereum.js
@@ -24,10 +24,10 @@ class Ethereum {
   }
 
   getBalance (walletAddress, callback) {
-    this.web3.eth.getBalance(address, callback)
+    this.web3.eth.getBalance(walletAddress, callback)
   }
 
-  transferAmount (fromAccount, value, callback) {
+  transferAmount (fromAccount, privatekey, value, callback) {
     // Need to move private key and gas limit
     let privateKey = new Buffer(privatekey, 'hex')
     let rawTx = {
@@ -38,17 +38,17 @@ class Ethereum {
       value: this.web3.toHex(this.web3.toWei(value, 'ether'))
     }
 
-    let tx = new Tx(rawTx);
-    tx.sign(privateKey);
+    let tx = new Tx(rawTx)
+    tx.sign(privateKey)
 
-    let serializedTx = tx.serialize();
+    let serializedTx = tx.serialize()
     // Return Tx Hash on succes
-    this.web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), callback);
+    this.web3.eth.sendRawTransaction('0x' + serializedTx.toString('hex'), callback)
   }
 
-  getNonce(fromAddress) {
-    let count = this.web3.eth.getTransactionCount(fromAddress);
-    return(this.web3.toHex(count))
+  getNonce (fromAddress) {
+    let count = this.web3.eth.getTransactionCount(fromAddress)
+    return (this.web3.toHex(count))
   };
 
   checkStatusOfTransaction (txHash, callback) {
--- src/services/web3.js
+++ src/services/web3.js
@@ -1,5 +1,5 @@
 import Web3 from 'web3'
 
-export default function setProvider(node_url) {
-  return (new Web3(new Web3.providers.HttpProvider(node_url)))
+export default function setProvider (url) {
+  return (new Web3(new Web3.providers.HttpProvider(url)))
 }
