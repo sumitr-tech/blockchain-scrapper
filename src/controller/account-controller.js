01744ff71c42a96f74dff17ea7e5f3a512449806
Sumit Rajput <sumitr@techracers.com>
2017-10-13 12:17:45 +0530

    Fixed issue related to saving of accounts in db

--- src/controller/account-controller.js
+++ src/controller/account-controller.js
@@ -1,17 +1,23 @@
 import Account from '../models/account'
+import {
+  EthereumBlockchain,
+  TokenEthereumBlockchain
+} from '../constants'
 
-const createDBAccount = (account, blockchain, mnemonicId) => {
-  const dbAccount = new Account({
-    walletAddress: account.address,
-    type: blockchain === EthereumBlockchain || blockchain === TokenEthereumBlockchain ? 'ETH' : 'BTC',
-    privateKeyEncrypted: account.privkey,
-    publicKey: account.pubkey,
-    mnemonicPath: account.index,
-    mnemonicRef: mnemonicId
-  })
-  dbAccount.save((error) => {
-    if (error) {
-      console.error(`Error in creation of Account for MnemonicId: ${mnemonicId} index: ${account.index}`)
-    }
+export const createDBAccount = (account, blockchain, mnemonicId) => {
+  setImmediate(() => {
+    const dbAccount = new Account({
+      walletAddress: account.address,
+      type: blockchain === EthereumBlockchain || blockchain === TokenEthereumBlockchain ? 'ETH' : 'BTC',
+      privateKeyEncrypted: account.privkey,
+      publicKey: account.pubkey,
+      mnemonicPath: account.index,
+      mnemonicRef: mnemonicId
+    })
+    dbAccount.save((error) => {
+      if (error) {
+        console.error(`Error in creation of Account for MnemonicId: ${mnemonicId} index: ${account.index}`)
+      }
+    })
   })
 }
