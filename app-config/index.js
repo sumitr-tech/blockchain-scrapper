865b67278b6454c58e1a84ac405e01720b5fd1ba
Sumit Rajput <sumitr@techracers.com>
2017-10-16 08:02:08 +0530

    Made changes in configuration file

--- app-config/index.js
+++ app-config/index.js
@@ -13,29 +13,17 @@ const config = convict({
     default: 'v&W8uK1M01&@$$ecGM@1#DU#Ipx7MV&CmThD@oPirrox3^Rgny50Eq*1MsCuVHdBWTzn07JQH6Qr#pAomfBPSdfgW$wx@%c*iif!',
     env: 'PRIV_KEY_ENC_SECRET'
   },
-  port: {
-    doc: 'The port to bind.',
-    format: 'port',
-    default: 3000,
-    env: 'PORT'
-  },
   db_url: {
     doc: 'Database host name/IP',
     format: '*',
-    default: 'mongodb://localhost/blockchainScrapper',
+    default: 'mongodb://<username>:<password>.mlab.com:17965/blockchain_scrapper',
     env: 'MONGO_URL'
   },
-  db_seed_admin_password: {
-    doc: 'This will be the password of the Admin User',
-    format: String,
-    default: '0s@7VlD#ss',
-    env: 'ADMIN_PASSWORD'
-  },
-  gas_limit_for_transactions: {
-    doc: 'Gas limit for transactions',
-    format: 'nat',
-    default: 3000000,
-    env: 'GAS_LIMIT_FOR_TRANSACTIONS'
+  mnemonic_phrase_1: {
+    doc: 'Mnemonic phrase',
+    formate: String,
+    default: '',
+    env: 'MNEMONIC_PHRASE_1'
   }
 })
 
--- scrapper-config/index.js
+++ scrapper-config/index.js
@@ -1,17 +1,18 @@
 import { EthereumBlockchain } from '../src/constants'
+import config from '../app-config'
 
 const scrapperConfig = [
   {
     mnemonicCustomName: 'Mnemonic1',
-    mnemonic: 'recall that despair search riot curve sail empower north furnace scheme frame sweet dizzy hockey',
-    passphrase: 'test',
+    mnemonic: config.get('mnemonic_phrase_1'),
+    passphrase: '',
     path: `m/44'/60'/0'/0`,
     lastIndex: 10,
-    initialBlock: 1847000,
+    initialBlock: 1875800,
     zValue: 10,
     blockchain: EthereumBlockchain,
-    toAddress: '0xA',
-    clientNode: 'http://clientnode'
+    toAddress: '0x6862932bf3082b7cA21ccb2d242080B0906A4E4d',
+    clientNode: 'http://localhost:8545'
   }
 ]
 
